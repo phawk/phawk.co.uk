2fff93310851f70b018c1c35a9a8e54556c7a724
Pete Hawkins <pete@alternatelabs.co>
2020-06-08 18:54:42 +0100

    fix(timeline): whitelist timeline selectors

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,13 +47,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Pete Hawkins on React`,
+        short_name: `phawk`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#38b2ac`,
+        theme_color: `#38b2ac`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
@@ -76,11 +76,8 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: false, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        ignore: [
-          "/ignored.css",
-          "vertical-timeline-component-for-react/Timeline.css",
-          "vertical-timeline-component-for-react/TimelineItem.css",
-        ], // Ignore files/folders
+        whitelistPatterns: [/^timeline/],
+        ignore: ["/ignored.css"], // Ignore files/folders
       },
     },
   ],
