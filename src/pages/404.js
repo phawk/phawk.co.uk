c608633b30e5d3cbc59c3715df4266cca0d26002
Pete Hawkins <pete@alternatelabs.co>
2020-06-09 15:32:21 +0100

    feat(404 page): update styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,15 @@ import React from "react"
 
 import Layout from "../components/Layouts/Base"
 import SEO from "../components/seo"
+import PageTitle from "../components/PageTitle"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <PageTitle>Whoops!</PageTitle>
+    <p className="text-xl">
+      You just hit a route that doesn&#39;t exist... the sadness.
+    </p>
   </Layout>
 )
 
