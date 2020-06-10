351d1406c70cdce95f33c0673d036c3a43e143a0
Pete Hawkins <pete@alternatelabs.co>
2020-06-10 18:24:46 +0100

    feat(projects): add first project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,11 +4,36 @@ import Layout from "../components/Layouts/Base"
 import SEO from "../components/seo"
 import PageTitle from "../components/PageTitle"
 
+import payhereIcon from "../images/payhere-icon.svg"
+
 const ProjectsPage = () => (
   <Layout>
     <SEO title="Projects" />
     <PageTitle>Projects</PageTitle>
-    <p className="text-xl">Coming soon.</p>
+
+    <div className="mt-16">
+      <a
+        href="https://payhere.co"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="group flex flex-col md:flex-row md:items-center border border-gray-300 hover:border-gray-400 rounded-lg mb-6 px-4 py-3 shadow-sm"
+      >
+        <img
+          src={payhereIcon}
+          alt="Payhere"
+          className="w-10 h-10 object-contain mb-4 md:mb-0 md:mr-4"
+        />
+        <div>
+          <h3 className="font-semibold text-gray-900 text-lg group-hover:underline">
+            Payhere
+          </h3>
+          <p className="text-base leading-snug">
+            Helps you modernize your business payments with easy subscriptions
+            and instalment plans, all in a simple payment link.
+          </p>
+        </div>
+      </a>
+    </div>
   </Layout>
 )
 
