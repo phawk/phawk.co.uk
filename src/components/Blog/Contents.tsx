10eb79b1c9d8d1a48d93a5acf32156c74cc3db1a
Pete Hawkins <pete@alternatelabs.co>
2020-09-25 21:33:26 +0100

    feat(ts): migrate blog Contents component to use typescript

--- src/components/Blog/Contents.tsx
+++ src/components/Blog/Contents.tsx
@@ -2,7 +2,11 @@ import { map } from "lodash"
 import React from "react"
 import { Link as ScrollLink } from "react-scroll"
 
-const Contents = ({ data }) => {
+type Props = {
+  data: Record<string, string>
+}
+
+const Contents: React.FC<Props> = ({ data }) => {
   return (
     <div className="my-10 border border-gray-300 rounded-md">
       <span className="block p-3 font-semibold border-b border-gray-300">
